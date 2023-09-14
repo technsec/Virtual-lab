1b7ffb5005f51b0848abc7ba9a978fb3b75a976d
technsec <117180785+technsec@users.noreply.github.com>
2023-09-14 11:17:36 -0500

    Update config-fw.py

--- config-fw.py
+++ config-fw.py
@@ -1,31 +1,9 @@
-# /*****************************************************************************
-# * Copyright (c) 2016, Palo Alto Networks. All rights reserved.              *
-# *                                                                           *
-# * This Software is the property of Palo Alto Networks. The Software and all *
-# * accompanying documentation are copyrighted.                               *
-# *****************************************************************************/
-#
-# Copyright 2016 Palo Alto Networks
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
 import time
 import shlex
 import subprocess
 import os
 import logging
-import urllib2
+import urllib.request
 import socket
 from socket import gethostname, gethostbyname
 import sys
@@ -44,7 +22,7 @@ MgmtIp = "10.100.0.4"
 #We know that DB IP is going to have x.x.4.5...so just need prefix
 DBServerIP = "10.100.4.5"
 #NAT FQDN
-nat_fqdn= "IPACWebPublicIP"
+nat_fqdn= "DatabaseServer"
 #The api key is pre-generated for  paloalto/Pal0Alt0@123
 api_key = "LUFRPT1CU0dMRHIrOWFET0JUNzNaTmRoYmkwdjBkWWM9alUvUjBFTTNEQm93Vmx0OVhFRlNkOXdJNmVwYWk5Zmw4bEs3NjgwMkh5QT0="
 
@@ -55,7 +33,10 @@ gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
 config_file_url = "https://raw.githubusercontent.com/technsec/Virtual-lab/main/"
 config_file_name = "azure-sample.xml"
 config_file= "https://raw.githubusercontent.com/technsec/Virtual-lab/main/azure-sample.xml"
-#curl_string = 'curl --form file=@%s --insecure "https://%s/api/?type=import&category=configuration&file-name=%s&key=%s"' % (config_file_name, MgmtIp, config_file_name, api_key)
+config_guess_sql_root_password= "https://raw.githubusercontent.com/technsec/Virtual-lab/main/guess-sql-root-password.cgi"
+config_ssh_to_db= "https://raw.githubusercontent.com/technsec/Virtual-lab/main/ssh-to-db.cgi"
+config_sql_attack= "https://raw.githubusercontent.com/technsec/Virtual-lab/main/sql-attack.html"
+curl_string = 'curl --form file=@%s --insecure "https://%s/api/?type=import&category=configuration&file-name=%s&key=%s"' % (config_file, MgmtIp, config_file, api_key)
 
 
 def main():
@@ -80,7 +61,7 @@ def load_and_commit_config():
 
     # Read the config file
     #with open(config_file_path, "r") as file:
-     file = urllib2.urlopen(config_file) 
+    file = urllib.request.urlopen(config_file) 
     config_content = file.read()
 
     # Send the request to load the configuration
@@ -147,32 +128,7 @@ def config_wp():
         logger.info("[ERROR]: gzip error")
         return 'false'
 
-
-    #Connect to database and see if it is up...if not...wait?
-    #i = 0
-    #while(i<10):
-    #    try:
-    #       p = subprocess.Popen(shlex.split("mysql -udemouser -ppaloalto@123 -h %s -e 'show databases'" % (DBServerIP)),stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    #        output = p.communicate()[0]
-    #    except:
-    #       logger.info("[ERROR]: When contacting database ".format(sys.exc_info()[0]))
-    #       return 'false'
-    #   if ("Can't connect to MySQL server" in output):
-    #       logger.info("[INFO]: Database not ready yet..will try again")
-    #       time.sleep(15)
-    #        i+=1
-    #       continue
-    #   elif ("Demo" in output):
-    #       logger.info("[INFO]: Database up!")
-    #       break
-    #    else:
-    #     logger.info("[ERROR]: Demo database not found. {}".format(output))
-    #       if (i<10):
-    #           i+=1
-    #            break
-    #        else:
-    #           return 'false'
-
+   
     #Then continue to finish wordpress setup
     #Just need a config file
     try:
@@ -187,8 +143,6 @@ def config_wp():
     except subprocess.CalledProcessError, e:
         logger.info("[ERROR]: setup-WP add user error {}".format(e))
         return 'false'
-
-
     try:
         subprocess.check_output(shlex.split("sed -i \"s/define('DB_PASSWORD'.*/define('DB_PASSWORD', 'paloalto@123');/g\" /etc/wordpress/config-%s.php" % (DBServerIP)))
     except subprocess.CalledProcessError, e:
@@ -206,8 +160,7 @@ def config_wp():
 
     #Download guess-password file
     try:
-        #subprocess.check_output(shlex.split("wget -O /usr/lib/cgi-bin/guess-sql-root-password.cgi https://%s.blob.core.windows.net/images/guess-sql-root-password.cgi"%(StorageAccountName)))
-        subprocess.check_output(shlex.split("wget -O /usr/lib/cgi-bin/guess-sql-root-password.cgi %sguess-sql-root-password.cgi"%(config_file_url)))
+        subprocess.check_output(shlex.split("wget -O /usr/lib/cgi-bin/guess-sql-root-password.cgi %sguess-sql-root-password.cgi"%(config_guess_sql_root_password)))
     except subprocess.CalledProcessError, e:
         logger.info("[ERROR]: wget guess-sql-root-password.cgi error {}".format(e))
         return 'false'
@@ -229,8 +182,7 @@ def config_wp():
 
     #Download ssh-to-db.cgi file
     try:
-        #subprocess.check_output(shlex.split("wget -O /usr/lib/cgi-bin/ssh-to-db.cgi https://%s.blob.core.windows.net/images/ssh-to-db.cgi"%(StorageAccountName)))
-        subprocess.check_output(shlex.split("wget -O /usr/lib/cgi-bin/ssh-to-db.cgi %sssh-to-db.cgi"%(config_file_url)))
+        subprocess.check_output(shlex.split("wget -O /usr/lib/cgi-bin/ssh-to-db.cgi %sssh-to-db.cgi"%(config_ssh_to_db)))
     except subprocess.CalledProcessError, e:
         logger.info("[ERROR]: wget ssh-to-db.cgi  {}".format(e))
         return 'false'
@@ -251,8 +203,7 @@ def config_wp():
 
     #Download sql-attack.html page
     try:
-        #subprocess.check_output(shlex.split("wget -O /var/www/html/sql-attack.html https://%s.blob.core.windows.net/images/sql-attack.html"%(StorageAccountName)))
-        subprocess.check_output(shlex.split("wget -O /var/www/html/sql-attack.html %ssql-attack.html"%(config_file_url)))
+        subprocess.check_output(shlex.split("wget -O /var/www/html/sql-attack.html %ssql-attack.html"%(config_sql_attack)))
     except subprocess.CalledProcessError, e:
         logger.info("[ERROR]: wget sql-attack.html error {}".format(e))
         return 'false'
